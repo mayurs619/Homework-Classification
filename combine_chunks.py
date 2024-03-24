9b665c52ae72f1769620ffca0d9147d8a0571429
Christopher Li <cl619@duke.edu>
2024-03-23 21:47:11 -0400

    model selection

--- combine_chunks.py
+++ combine_chunks.py
@@ -1,18 +1,21 @@
 import os
 import shutil
+import splitfolders
 
-def merge_all(dir):
-    merged_dir = os.path.join(dir, "merged")
-    if not os.path.exists(merged_dir):
-        os.mkdir(merged_dir)
-    for dir_name in os.listdir(dir):
-        dir_path = os.path.join(dir, dir_name)
+def merge_all(dir, class_name, dst):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    class_path = os.path.join(dir, class_name)
+    for dir_name in os.listdir(class_path):
+        dir_path = os.path.join(dir, class_name, dir_name)
         for file_name in os.listdir(dir_path):
-            file_path = os.path.join(dir, dir_name, file_name)
-            new_file_path = os.path.join(merged_dir, file_name)
+            file_path = os.path.join(dir, class_name, dir_name, file_name)
+            new_file_path = os.path.join(dst, file_name)
             if os.path.exists(new_file_path):
                 continue
             shutil.copyfile(file_path, new_file_path)
 
-merge_all('data/hw')
-merge_all('data/no_hw')
+merge_all('data', 'hw', 'data/merged/hw')
+merge_all('data', 'no_hw', 'data/merged/no_hw')
+
+splitfolders.ratio('data/merged', output="data/split", seed=1337, ratio=(.8, 0.1,0.1)) 
